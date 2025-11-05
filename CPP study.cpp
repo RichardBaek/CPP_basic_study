8604124139e52c48d02ca8e291d7b97b84d636d3
RichardBaek <richardbaek22@Gmail.com>
2025-11-04 23:49:42 -0500

    day 1

--- CPP study.cpp
+++ CPP study.cpp
@@ -1,12 +1,28 @@
-﻿// CPP study.cpp : Defines the entry point for the application.
-//
+﻿#include "CPP study.h"
 
-#include "CPP study.h"
 
-using namespace std;
+void basic_output() {
+    int age = 20;
+    std::cout << "I'm " << age << " years old" << std::endl;
+}
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+void basic_input() {
+    int age;
+    std::cout << "How old are you then? ";
+    std::cin >> age;
+    std::cout << "You a " << age << " years old fossil." << std::endl;
+}
+
+void use_auto() {
+    auto max_students = 50;
+    auto class_average = 88.5;
+    
+    std::cout << "Max students: " << max_students << std::endl;
+    std::cout << "Class average: " << class_average << std::endl;
+}
+
+int main() {
+    basic_output();
+    basic_input();
+    use_auto();
 }
