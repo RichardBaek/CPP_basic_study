ae3345cfcdf3048aac4c4ef30a1bcc4466fb04d0
RichardBaek <richardbaek22@Gmail.com>
2025-11-04 23:40:29 -0500

    Add project files.

---
+++ CPP study.cpp
@@ -0,0 +1,12 @@
+﻿// CPP study.cpp : Defines the entry point for the application.
+//
+
+#include "CPP study.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
