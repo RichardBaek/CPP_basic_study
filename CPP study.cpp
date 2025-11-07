a4fd50754b271b6feaae270043038f42215071fd
RichardBaek <richardbaek22@Gmail.com>
2025-11-06 22:59:22 -0500

    Nov 6 2025

--- CPP study.cpp
+++ CPP study.cpp
@@ -21,8 +21,34 @@ void use_auto() {
     std::cout << "Class average: " << class_average << std::endl;
 }
 
+enum class TrafficLight {
+    RED,
+    YELLOW,
+    GREEN
+};
+
+int distance(int speed, int time) {
+    return speed * time;
+}
+
+bool can_stop(TrafficLight light, int distance) {
+    bool stop = false;
+    stop = (light != TrafficLight::GREEN) && (distance > 500);
+    return stop;
+
+}
+
+
 int main() {
     basic_output();
     basic_input();
     use_auto();
+
+    int speed = 55;
+    int time_required = 100;
+    int dt = distance(speed, time_required);
+    TrafficLight light = TrafficLight::RED;
+    bool isstop = can_stop(light, dt);
+    std::cout << (isstop ? "Yes" : "No") << "\n";
+    return 0;
 }
